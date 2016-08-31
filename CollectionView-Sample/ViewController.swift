62bd3c9ced47c43b4ddf303e1d1d00207bd9c61c
新井 雅人 <arai_masato@waku-2.com>
2016-08-31 15:13:51 +0900

    Project作成

---
+++ CollectionView-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView-Sample
+//
+//  Created by 新井　雅人 on 2016/08/31.
+//  Copyright © 2016年 Tea and Coffee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
